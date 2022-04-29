1fdbb0a90bbc248bf0ce1f3f3192aae7d12aed88
MuhammadHafijur <hafijur.salah@gmail.com>
2022-04-29 17:42:12 +0600

    contact page added

---
+++ pages/contact/index.js
@@ -0,0 +1,165 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="bg-white">
+      {/* <section className="text-gray-600 body-font relative">
+        <div className="absolute inset-0 bg-gray-300">
+          <iframe
+            className="contrast-125 grayscale opacity-40"
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            marginHeight="0"
+            marginWidth="0"
+            title="map"
+            scrolling="no"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13721.014069318007!2d76.6922005!3d30.7112726!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x214b2cbd35685f0d!2sBase%202%20Brand%20Infotech%20Pvt.%20Ltd.!5e0!3m2!1sen!2sbd!4v1651227040903!5m2!1sen!2sbd"
+          ></iframe>
+        </div>
+        <div className="container px-5 py-24 mx-auto flex">
+          <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
+            <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
+              Feedback
+            </h2>
+            <p className="leading-relaxed mb-5 text-gray-600">
+              Post-ironic portland shabby chic echo park, banjo fashion axe
+            </p>
+            <div className="relative mb-4">
+              <label
+                htmlhtmlFor="email"
+                className="leading-7 text-sm text-gray-600"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div className="relative mb-4">
+              <label
+                htmlhtmlFor="message"
+                className="leading-7 text-sm text-gray-600"
+              >
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              ></textarea>
+            </div>
+            <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              Submit
+            </button>
+            <p className="text-xs text-gray-500 mt-3">
+              Chicharrones blog helvetica normcore iceland tousled brook viral
+              artisan.
+            </p>
+          </div>
+        </div>
+      </section> */}
+
+      {/*  */}
+      <div className="container px-5 py-12 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <iframe
+            width="100%"
+            height="100%"
+            className="absolute inset-0"
+            frameBorder="0"
+            title="map"
+            marginHeight="0"
+            marginWidth="0"
+            scrolling="no"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13721.014069318007!2d76.6922005!3d30.7112726!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x214b2cbd35685f0d!2sBase%202%20Brand%20Infotech%20Pvt.%20Ltd.!5e0!3m2!1sen!2sbd!4v1651227040903!5m2!1sen!2sbd"
+            style={{ filter: "grayscale(1) contrast(1.2) opacity(0.4)" }}
+          ></iframe>
+          <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                ADDRESS
+              </h2>
+              <p className="mt-1">
+                F-209, Industrial Area, Sector 74, Sahibzada Ajit Singh Nagar,
+                Punjab 160074, India
+              </p>
+            </div>
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                EMAIL
+              </h2>
+              <a className="text-indigo-500 text-lg font-poppins leading-relaxed">
+                tech@base2brand.com
+              </a>
+              <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
+                PHONE
+              </h2>
+              <p className="leading-relaxed text-lg">+918360116967</p>
+            </div>
+          </div>
+        </div>
+        <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
+            Contact Us
+          </h2>
+          <p className="leading-relaxed mb-5 text-gray-600">
+            We&apos;d love your feedback.
+          </p>
+          <div className="relative mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-600">
+              Name
+            </label>
+            <input
+              required
+              type="text"
+              id="name"
+              name="name"
+              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+              Email
+            </label>
+            <input
+              required
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="message"
+              className="leading-7 text-sm text-gray-600"
+            >
+              Message
+            </label>
+            <textarea
+              required
+              id="message"
+              name="message"
+              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 font-semibold rounded text-lg"
+          >
+            Submit
+          </button>
+          <p className="text-xs text-gray-500 mt-3">
+            Feel free to contact us and lets discuss your dream.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- pages/marketing/checkupmeds.js
+++ pages/marketing/checkupmeds.js
@@ -8,7 +8,7 @@ const CheckUpMeds = () => {
   return (
     <div className="bg-[#D5F8FD] md:border-l-[22px] border-[#13436B]">
       <div className="w-[92%] flex h-screen mx-auto">
-        <div className="flex m-auto flex-col md:flex-row min-w-full justify-items-end items-center ">
+        <div className="flex my-auto flex-col md:flex-row min-w-full justify-items-end items-center ">
           <div className="md:w-1/2 order-last md:order-none">
             <h1 className="font-nanummy text-[20px] sm:text-[40px] md:text-[50px] 2xl:text-[100px]  4k:text-[155px] text-center md:text-left font-extrabold text-[#022C5B]">
               Checkups <br /> Medical Hub
